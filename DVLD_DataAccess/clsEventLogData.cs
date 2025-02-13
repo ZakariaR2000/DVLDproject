ba58c6be6ffa16a98f088bd5d43ebf49f4b48c2e
ZakariaR2000 <162996600+ZakariaR2000@users.noreply.github.com>
2025-02-13 09:23:06 +0300

    Add files via upload

---
+++ DVLD_DataAccess/clsEventLogData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsEventLogData
+    {
+        public static void CheckError(string SourceName, string ErrorMessage,
+            string ErrorCategory = "Application")
+        {
+
+            if (!EventLog.SourceExists(SourceName))
+            {
+                EventLog.CreateEventSource(SourceName, ErrorCategory);
+            }
+            EventLog.WriteEntry(SourceName, ErrorMessage, EventLogEntryType.Information);
+        }
+    }
+}
